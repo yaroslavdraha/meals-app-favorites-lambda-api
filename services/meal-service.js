902a6c3d67d7450fdf324bf83d87486678a79b61
Yaroslav Draga <yaroslav@spheremall.com>
2020-06-10 21:28:10 +0300

    add some db db code

--- services/meal-service.js
+++ services/meal-service.js
@@ -1,3 +1,31 @@
-module.exports.getMeals = () => {
+
+
+
+const getMeals = () => {
   return [1,2,3];
+};
+
+const addFavorite = (mealId, userId = 'default') => {
+
+  const params = {
+    Item: {
+      "mealId": {
+        S: mealId
+      },
+      "userId": {
+        S: userId
+      }
+    },
+    TableName: 'ma-favorites'
+  }
+
+  dynamoDB.putItem(params, (err, data) => {
+
+  });
+
+  return [1,2,3];
+};
+
+module.exports = {
+  getMeals
 }
--- services/meal-service.test.js
+++ services/meal-service.test.js
@@ -1,4 +1,4 @@
-const {getMeals} = require('./services/meal-service');
+const {getMeals} = require('./meal-service');
 
 test('test meals', () => {
   expect(getMeals()).toEqual([1,2,3]);
